75fdc178fc13a094cfdd79389dc3b383abcc2380
Spoorti Kalakappa Dyampur <151918055+spoorti-k-d@users.noreply.github.com>
2026-06-07 13:13:34 +0530

    ✨ DebtIQ™ Automated Code Refactor: frontend/eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,43 +1,14 @@
-import { defineConfig, globalIgnores } from '@eslint/config'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from '@eslint/config'
 
-export default defineConfig([
-  globalIgnores([
-    {
-      path: 'dist',
-      glob: '**/*',
-    },
-    {
-      path: 'node_modules',
-      glob: '**/*',
-    },
-  ]),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      'plugin:react-hooks/recommended',
-      'plugin:react-refresh/recommended',
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parserOptions: {
-        ecmaVersion: 2020,
-      },
-    },
-    rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error',
-      'react/prop-types': 'error',
-      'react-refresh/only-export-components': [
-        'error',
-        { allowConstantExport: true },
-      ],
-    },
+export default defineConfig({
+  ignorePatterns: ['dist', 'node_modules'],
+  extends: [
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+  ],
+  rules: {
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+    'react/prop-types': 'error',
   },
-])
+})
