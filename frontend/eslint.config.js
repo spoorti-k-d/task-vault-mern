0f6c6d7733af7a980ea0941f15bcb18dc8af2978
Spoorti Kalakappa Dyampur <151918055+spoorti-k-d@users.noreply.github.com>
2026-06-07 13:14:33 +0530

    ✨ DebtIQ™ Automated Code Refactor: frontend/eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from '@eslint/config'
 export default defineConfig({
   ignorePatterns: ['dist', 'node_modules'],
   extends: [
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react-refresh/recommended',
   ],
